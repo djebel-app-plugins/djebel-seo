d4ca856fb546b46ef1ba15cf57a4bcb2c45b053f
Slavi Marinov <admin@orbisius.com>
2025-03-17 22:52:25 +0200

    dj seo

---
+++ plugin.php
@@ -0,0 +1,29 @@
+<?php
+/*
+plugin_name: Djebel SEO
+plugin_uri: https://djebel.com/plugins/djebel-seo
+description: Renders SEO meta tags.
+version: 1.0.0
+load_priority:20
+tags: seo, meta, tags
+stable_version: 1.0.0
+min_php_ver: 5.6
+min_dj_cms_ver: 1.0.0
+tested_with_dj_cms_ver: 1.0.0
+author_name: Svetoslav Marinov (Slavi)
+company_name: Orbisius
+author_uri: https://orbisius.com
+text_domain: hello-world
+license: gpl2
+*/
+
+$obj = new Djebel_SEO();
+Dj_CMS_Hooks::addAction( 'app.page.html.head', [ $obj, 'renderMetaData', ] );
+
+class Djebel_SEO
+{
+    public function renderMetaData()
+    {
+
+    }
+}
