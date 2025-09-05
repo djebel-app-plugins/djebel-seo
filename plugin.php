e13df34ffc07477eef4de83781b096582e1aa45b
slavi <admin@orbisius.com>
2025-09-05 19:31:36 +0300

    moved prepareMetaData up so it's clear that it's doing some extra work before the rendering. simplified code

--- plugin.php
+++ plugin.php
@@ -23,15 +23,60 @@ Dj_App_Hooks::addFilter( 'app.page.full_content', [ $obj, 'updateMeta' ], 50 );
 
 class Djebel_SEO
 {
-    public function updateMeta($content)
+    /**
+     * Decides what meta info to use for the page early on.
+     * @return void
+     * @throws Exception
+     */
+    public function prepareMetaData()
     {
+        $req_obj = Dj_App_Request::getInstance();
+        $options_obj = Dj_App_Options::getInstance();
+
+        $segments = $req_obj->segments();
+
+        // home page?
+        if (empty($segments)) {
+            $meta_title = $options_obj->get('meta.home.title');
+            $meta_description = $options_obj->get('meta.home.description');
+            $meta_keywords = $options_obj->get('meta.home.keywords');
+        } else {
+            // loop through the segments and start with the last one that's the current page.
+            // if it has seo meta data then use it otherwise use the parent page
+            $reverse_segments = array_reverse($segments);
+
+            foreach ($reverse_segments as $segment) {
+                $page_link = $segment;
+                $meta_title = $options_obj->get("meta.{$page_link}.title");
+                $meta_description = $options_obj->get("meta.{$page_link}.description");
+                $meta_keywords = $options_obj->get("meta.{$page_link}.keywords");
+
+                if (!empty($meta_title) || !empty($meta_description)) {
+                    break;
+                }
+            }
+        }
+
         $page_obj = Dj_App_Page::getInstance();
         $options_obj = Dj_App_Options::getInstance();
 
+        $meta_title = empty($meta_title) ? $options_obj->meta->default->title : $meta_title;
+        $meta_description = empty($meta_description) ? $options_obj->meta->default->description : $meta_description;
+        $meta_keywords = empty($meta_keywords) ? $options_obj->meta->default->keywords : $meta_keywords;
+
+        $page_obj->meta_title = $meta_title;
+        $page_obj->meta_description = $meta_description;
+        $page_obj->meta_keywords = $meta_keywords;
+    }
+
+    public function updateMeta($content)
+    {
+        $page_obj = Dj_App_Page::getInstance();
+
         $fields = [
-            'title' => $options_obj->meta->default->title,
-            'keywords' => $options_obj->meta->default->keywords,
-            'description' => $options_obj->meta->default->description,
+            'title' => $page_obj->meta_title,
+            'keywords' => $page_obj->meta_keywords,
+            'description' => $page_obj->meta_description,
         ];
 
         $ctx = [];
@@ -57,64 +102,5 @@ class Djebel_SEO
         }
 
         return $content;
-    }
-
-    public function prepareMetaData()
-    {
-        $req_obj = Dj_App_Request::getInstance();
-        $options_obj = Dj_App_Options::getInstance();
-
-        $segments = $req_obj->segments();
-
-        // home page?
-        if (empty($segments)) {
-            // Check for home page meta first, fall back to default
-            $meta_title = $options_obj->get('meta.home.title');
-
-            if (empty($meta_title)) {
-                $meta_title = $options_obj->get('meta.default.title');
-            }
-            
-            $meta_description = $options_obj->get('meta.home.description');
-
-            if (empty($meta_description)) {
-                $meta_description = $options_obj->get('meta.default.description');
-            }
-            
-            $meta_keywords = $options_obj->get('meta.home.keywords');
-
-            if (empty($meta_keywords)) {
-                $meta_keywords = $options_obj->get('meta.default.keywords');
-            }
-        } else {
-            // loop through the segments and start with the last one that's the current page.
-            // if it has seo meta data then use it otherwise use the parent page
-            $reverse_segments = array_reverse($segments);
-
-            foreach ($reverse_segments as $segment) {
-                $page_link = $segment;
-                $meta_title = $options_obj->get("meta.{$page_link}.title");
-                $meta_description = $options_obj->get("meta.{$page_link}.description");
-                $meta_keywords = $options_obj->get("meta.{$page_link}.keywords");
-
-                if (!empty($meta_title) || !empty($meta_description)) {
-                    break;
-                }
-            }
-        }
-
-        $page_obj = Dj_App_Page::getInstance();
-
-        if (!empty($meta_title)) {
-            $page_obj->meta_title = $meta_title;
-        }
-
-        if (!empty($meta_description)) {
-            $page_obj->meta_description = $meta_description;
-        }
-
-        if (!empty($meta_keywords)) {
-            $page_obj->meta_keywords = $meta_keywords;
-        }
     }
 }
